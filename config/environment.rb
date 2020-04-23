6510b88b7d4cf5c191d5cae42678410b0985092e
Hitoshi-unten <untenht@gmail.com>
2020-04-23 12:25:22 +0000

    Finish japanization-and-time-format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+WillPaginate::ViewHelper.pagination_options[:previous_label] = '前ページ'
+WillPaginate::ViewHelper.pagination_options[:next_label] = '次ページ'
