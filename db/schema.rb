ab46082dc68b8f3448c57e09af9c28c3c39127bf
Hitoshi-unten <untenht@gmail.com>
2020-04-26 15:41:20 +0000

    Finish japanization-and-time-fomat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200423073906) do
+ActiveRecord::Schema.define(version: 20200426100833) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
