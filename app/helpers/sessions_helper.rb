dca532f363a784d900f2c79395c43a0e7d14c73a
Hitoshi-unten <untenht@gmail.com>
2020-04-26 08:25:10 +0000

    Finish editing-users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,15 +6,15 @@ module SessionsHelper
 
   def remember(user)
     user.remember
-    cookies.parmanent.signed[:user_id] = user.id
-    cookies.parmanent[:remember_token] = user.remember_token
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
   
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
-  end    
+  end
   
   def log_out
     forget(current_user)
@@ -27,14 +27,27 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticate?(cookies[:remember_token])
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def logged_in?
     !current_user.nil?
-  end  
+  end
+  
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get? 
+  end
 end
